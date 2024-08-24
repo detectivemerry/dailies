f3c838902e1f0feb60de4a1b85de280669674460
SebastinGoh <gmcsebastin@gmail.com>
2024-08-24 14:40:25 +0800

    Update dbschema to be defined in model.ts only

--- types/model.ts
+++ types/model.ts
@@ -1,15 +1,24 @@
 import { ObjectId } from "mongodb";
 
-export interface Goal {
-  goal_name : string;
-  goal_id : ObjectId;
-  no_of_members : Number;
-  avatarIcon : string;
+export interface Users {
+    _id : ObjectId; //'1a2b3c4d5e'
+    firstName : string; //"John"
+    lastName : string; //"Smith"
+    username : string; //"Js1909"
+    email : string; //"js1909@gmail.com"
+    password : string; //"!@Qw#$5rer234cw5#$%regtg2"
 }
 
-export interface GoalType {
-  _id: ObjectId;
-  name: string;
-  avatarIcon: string;
-  goals : Goal[];
+export interface GoalTypes {
+    _id : ObjectId; //'1a2b3c4d5e'
+    name : string; //"Fitness"
+    icon : string; //"FITNESS"
+    goals : [
+        {
+            _id : ObjectId; //'1a2b3c4d5e'
+            name : string; //"Gym"
+            icon : string; //"GYM"
+            no_of_members : number; //123
+        }
+    ]
 }
