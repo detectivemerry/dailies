f9288026f932cdf279046526b6b6c8a0df117840
SebastinGoh <gmcsebastin@gmail.com>
2024-11-07 21:32:02 +0800

    Update home page with some content

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Post, GoalType, Goal } from "@/types/model";
+import GoalTag from "@/components/goal/GoalTag";
+import SectionHeader from "@/components/header/SectionHeader";
+import PostCard from "@/components/post/PostCard";
 
-export default function page() {
+export default async function page() {
+
+  const goalsTypesResponse = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/goals/`, {
+    method : "GET",
+    headers : {},
+    cache: "no-store",
+  })
+
+  const { data : goalTypes } = await goalsTypesResponse.json();
+  const goals:Goal[] = goalTypes.reduce((acc:Goal[], goalType:GoalType) => acc.concat(goalType.goals), []);
+  // Sort goals based on no_of_members and take top 10
+  goals.sort((a:Goal, b:Goal) => b.no_of_members - a.no_of_members).splice(5); 
+  
+  const postsResponse = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/posts/`, {
+    method: "GET",
+    headers : {},
+    cache: "no-store",
+  });
+
+  const { data : posts } = await postsResponse.json();
+
   return (
-    <div>home page</div>
+    <div>page</div>
   )
 }
