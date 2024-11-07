4b7ad640c10f9aabdd47499ac449180ebcb19739
detectivemerry <jiiasheng@hotmail.com>
2024-11-07 21:23:10 +0800

    add styling

--- components/post/PostCard.tsx
+++ components/post/PostCard.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { Post } from "@/types/model";
 import React, { useState, useEffect, MouseEvent } from "react";
-import { AccessTime, CheckCircle, Edit, Replay } from "@mui/icons-material";
+import {
+  AccessTime,
+  CheckCircle,
+  Circle,
+  Edit,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  Replay,
+} from "@mui/icons-material";
 import { useSession } from "next-auth/react";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -49,18 +57,37 @@ export default function PostCard({ post }: PostCardProps) {
     return `${diffInYears} years left`;
   };
 
+  const computeTimeSincePosted = (time: Dayjs): string => {
+    const now = dayjs();
+    const diffInMinutes = now.diff(time, "minute");
+    const diffInHours = now.diff(time, "hour");
+    const diffInDays = now.diff(time, "day");
+
+    if (diffInMinutes < 60) {
+      return `${diffInMinutes}m`; // e.g., "42m"
+    } else if (diffInHours < 24) {
+      return `${diffInHours}h`; // e.g., "3h"
+    } else {
+      return `${diffInDays}d`; // e.g., "2d"
+    }
+  };
+
   return (
-    <div className="flex flex-col px-2 border-b">
+    <div className="flex flex-col px-2 border-b text-sm">
       <div className="flex justify-between px-2">
-        <div className="flex gap-3 my-3">
+        <div className="flex gap-2 my-3">
           <Link href={`/profile/${post.username}`} className="no-underline">
             <div className="font-bold text-main">{post.username}</div>
           </Link>
           <Link href={`/community/${post.goalName}`} className="no-underline">
-            <div className="bg-secondary rounded-2xl px-3 text-main">
+            <div className="bg-secondaryDark rounded-2xl px-3 text-main">
               {post.goalName}
             </div>
           </Link>
+          <div className="text-secondaryText flex gap-2 items-center">
+            <Circle sx={{ color: "#838383", fontSize: "0.25rem" }} />
+            {computeTimeSincePosted(dayjs(post.postedDateTime))}
+          </div>
         </div>
         {session?.user.username === post.username && (
           <div className="px-3 py-2" onClick={handleNavigateToEditPost}>
@@ -81,60 +108,84 @@ export default function PostCard({ post }: PostCardProps) {
       </div>
       <div>
         <div className="flex flex-col">
-          <div className="flex pt-3" onClick={handleExpand}>
+          <div className="flex pt-3 px-3" onClick={handleExpand}>
             <span className={`${expanded ? "" : "truncate"} px-2`}>
-              <Link href={`/profile/${post.username}`} className="no-underline">
-                <span className="font-bold text-main">{post.username}</span>{" "}
-              </Link>
               {post.caption}
             </span>
           </div>
+          {!expanded && (
+            <>
+              <div className="flex flex-col gap-3 m-3 mb-0 p-3 pb-4 border border-mainDisabled rounded-xl">
+                <Link
+                  href={`/profile/${post.username}`}
+                  className="no-underline"
+                >
+                  <div className="text-main">Goal: {post.userGoalName}</div>
+                </Link>
+              </div>
+              <div className="flex justify-center m-3" onClick={handleExpand}>
+                <KeyboardArrowDown
+                  sx={{ fontSize: "1.75rem", color: "#98aacd" }}
+                />
+              </div>
+            </>
+          )}
           {expanded && (
-            <div className="flex flex-col gap-3 m-3 mb-0 p-3 pb-4 border border-mainDisabled rounded-xl">
-              <Link href = {`/profile/${post.username}`} className = "no-underline">
-                <div className="text-main">{post.userGoalName}</div>
-              </Link>
-              <div className="flex gap-3">
-                <div className="bg-lightGray rounded-2xl px-3 flex gap-1">
-                  <div>
-                    {displayTimeLeftForGoal() === "finished" ? (
-                      <CheckCircle
-                        sx={{
-                          color: "#1D5D9B",
-                          fontSize: "1rem",
-                          marginTop: "-3px",
-                        }}
-                      />
-                    ) : post.goalEndDate ? (
-                      <AccessTime
-                        sx={{
-                          color: "#1D5D9B",
-                          fontSize: "1rem",
-                          marginTop: "-3px",
-                        }}
-                      />
-                    ) : (
-                      <Replay
-                        sx={{
-                          color: "#1D5D9B",
-                          fontSize: "1rem",
-                          marginTop: "-3px",
-                        }}
-                      />
-                    )}
+            <>
+              <div className="flex flex-col gap-3 m-3 mb-0 p-3 pb-4 border border-mainDisabled rounded-xl">
+                <Link
+                  href={`/profile/${post.username}`}
+                  className="no-underline"
+                >
+                  <div className="text-main">Goal: {post.userGoalName}</div>
+                </Link>
+                <div className="flex gap-3">
+                  <div className="bg-lightGray rounded-2xl px-3 flex gap-1">
+                    <div>
+                      {displayTimeLeftForGoal() === "finished" ? (
+                        <CheckCircle
+                          sx={{
+                            color: "#1D5D9B",
+                            fontSize: "1rem",
+                            marginTop: "-3px",
+                          }}
+                        />
+                      ) : post.goalEndDate ? (
+                        <AccessTime
+                          sx={{
+                            color: "#1D5D9B",
+                            fontSize: "1rem",
+                            marginTop: "-3px",
+                          }}
+                        />
+                      ) : (
+                        <Replay
+                          sx={{
+                            color: "#1D5D9B",
+                            fontSize: "1rem",
+                            marginTop: "-3px",
+                          }}
+                        />
+                      )}
+                    </div>
+                    <div>{displayTimeLeftForGoal()}</div>
                   </div>
-                  <div>{displayTimeLeftForGoal()}</div>
-                </div>
-                <div className="bg-lightGray rounded-2xl px-3">
-                  {post.frequencyCount} times {post.frequencyPeriod}
+                  <div className="bg-lightGray rounded-2xl px-3">
+                    {post.frequencyCount} times {post.frequencyPeriod}
+                  </div>
                 </div>
               </div>
-            </div>
+              <div className="text-secondaryText pt-3 pl-3">
+                {displayDate} {post.editedDateTime ? "(edited)" : ""}
+              </div>
+              <div className="flex justify-center m-3" onClick={handleExpand}>
+                <KeyboardArrowUp
+                  sx={{ fontSize: "1.75rem", color: "#98aacd" }}
+                />
+              </div>
+            </>
           )}
         </div>
-      </div>
-      <div className="text-secondaryText p-2">
-        {displayDate} {post.editedDateTime ? "(edited)" : ""}
       </div>
     </div>
   );
